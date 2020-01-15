66f3adb599411b43d0e95dfd581a3c7dbde32941
zxtheo <1709869@brunel.ac.uk>
2020-01-15 19:25:37 +0000

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,20 @@
+﻿''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'
+'Beadifier, a program to turn beads And photos into patterns
+'
+'This program Is free software: you can redistribute it and/or modify 
+'it under the terms Of the GNU General Public License As published by 
+'the Free Software Foundation, either version 3 Of the License, 
+'Or any later version. 
+'
+'Copyright(C) 2020
+'Author: Holly Everett 
+'
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
